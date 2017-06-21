f59ab47c8c1f2950097df83427935f4244c04ce9
José Tomás González <jtgonzalez@miuandes.cl>
2017-06-21 09:30:04 -0400

    Devise:
    * Added "first_name" and "last_name" fields to form.
    * Added "first_name" and "last_name" fields to permitted parameters.
    
    Users: Added validations for "first_name", "last_name" and "email".
    
    Games: Added validations for "title", "description" and "image_url".
    
    Added custom validator for "email".

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,15 @@
 class Game < ApplicationRecord
+  
+  validates :title, {
+    presence: true,
+    length: {minimum: 10, maximum: 30},
+    uniqueness: true
+  }
+  validates :description, {
+    presence: true,
+    length: {minimum: 10}
+  }
+  validates :image_url, {
+    presence: true
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, {
+    presence: true,
+    length: {minimum: 3, maximum: 30}
+  }
+  validates :last_name, {
+    presence: true,
+    length: {minimum: 3, maximum: 30}
+  }
+  validates :email, {
+    presence: true,
+    length: {maximum: 30},
+    uniqueness: true,
+    email: true
+  }
 end
